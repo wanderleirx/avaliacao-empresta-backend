e5964ba2f29895bcbfcc29b9b24dffa8979858ad
wma <wanderleirx@gmail.com>
2020-10-22 21:13:44 -0300

    Criado recurso instituições

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('App\\Http\\Controllers')->group(function (){
+    Route::get('/instituicoes', 'InstituicoesController@find');
+});
+
