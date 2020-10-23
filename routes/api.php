65b216e9f1f740b4d3ebfc82d718b2f96ce7e280
wma <wanderleirx@gmail.com>
2020-10-22 21:17:02 -0300

    Criado recurso convenios

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::namespace('App\\Http\\Controllers')->group(function (){
     Route::get('/instituicoes', 'InstituicoesController@find');
+    Route::get('/convenios', 'ConveniosController@find');
 });
 
