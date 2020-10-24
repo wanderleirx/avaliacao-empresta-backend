a11c67ce856efd489dfbc9304595a1d53d5dafb2
wma <wanderleirx@gmail.com>
2020-10-23 21:46:00 -0300

    finalizando API

--- app/Http/Controllers/SimulacaoController.php
+++ app/Http/Controllers/SimulacaoController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SimulacaoRequest;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class SimulacaoController extends Controller
 {
@@ -14,58 +12,60 @@ class SimulacaoController extends Controller
             $valor_emprestimo = $request->valor_emprestimo;
             $instituicoes = $request->instituicoes;
             $convenios = $request->convenios;
-            $parcela = $request->parcela;
-            
+            $parcelas = $request->parcelas;
             $simulacoes = $this->simular($valor_emprestimo);
-            
-            $data = [];
-            
-            foreach($simulacoes as $index => $value) {
-                if(empty($instituicoes) || array_search($index, $instituicoes) !== false) {
-                    var_dump($value);die;
-                    $data[$index] = $value;
-                }
-            }
-            var_dump($data);die;
-            
-            // if(empty($convenios) || array_search($value['convenio'], $convenios) !== false) {
-            //     $data[$index][] = $value;
-            // }
-            
-            
-            return response()->json([''], 200);
+            $simulacoes = $this->filtrar($simulacoes, $instituicoes, $convenios, $parcelas);
+            return response()->json($simulacoes, 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    
+
     private function getTaxasInstituicoes()
     {
         return json_decode(file_get_contents(storage_path('app/public/Models/taxas_instituicoes.json')), true);
     }
-    
+
     private function simular($valor_emprestimo)
     {
         $simulacoes = [];
         $taxas = $this->getTaxasInstituicoes();
-        
+
         foreach($taxas as $taxa) {
             $instituicao = $taxa['instituicao'];
 
             if(!isset($simulacoes[$instituicao])) {
                 $simulacoes[$instituicao] = [];
             }
-            
-                
+
             $simulacoes[$instituicao][] = [
                 'taxa' => $taxa['taxaJuros'],
                 'parcelas' => $taxa['parcelas'],
                 'valor_parcela' => round($valor_emprestimo * $taxa['coeficiente'], 2),
                 'convenio' => $taxa['convenio'],
             ];
-            
         }
-        
+
+        return $simulacoes;
+    }
+
+    private function filtrar($simulacoes, $instituicoes, $convenios, $parcelas)
+    {
+        foreach($simulacoes as $instituicao => &$value) {
+            foreach ($value as $index2 => $simulacao) {
+                if(!empty($convenios) && !is_numeric(array_search($simulacao['convenio'], $convenios)) ) {
+                    unset($value[$index2]);
+                }
+                if($parcelas && $parcelas != $simulacao['parcelas']) {
+                    unset($value[$index2]);
+                }
+            }
+            if(!empty($instituicoes) && !is_numeric(array_search($instituicao, $instituicoes))
+                || empty($simulacoes[$instituicao])) {
+                unset($simulacoes[$instituicao]);
+            }
+        }
+
         return $simulacoes;
     }
 }
